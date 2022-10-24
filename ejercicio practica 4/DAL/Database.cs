81f1836cf540ba7377d853cc7e9655707f457003
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2022-10-23 20:11:29 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio practica 4/DAL/Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using ejercicio_practica_4.Properties;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ejercicio_practica_4.DAL
+{
+    public class Database
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+
+        public bool testConection()
+        {
+            SqlConnection Con = this.getConnection();
+            Con.Open();
+            if (Con.State == System.Data.ConnectionState.Open)
+            {
+                Con.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
---
+++ ejercicio practica 4/PL/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace ejercicio_practica_4.PL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Test
+            // 
+            this.btn_Test.Location = new System.Drawing.Point(33, 31);
+            this.btn_Test.Name = "btn_Test";
+            this.btn_Test.Size = new System.Drawing.Size(169, 108);
+            this.btn_Test.TabIndex = 1;
+            this.btn_Test.Text = "Test";
+            this.btn_Test.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 185);
+            this.Controls.Add(this.btn_Test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Test;
+    }
+}
